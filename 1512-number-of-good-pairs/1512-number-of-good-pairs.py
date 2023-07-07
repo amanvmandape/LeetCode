c8aa285169d722f6f3d218e15a63831374a879d9
Aman Mandape <95904603+amanvmandape@users.noreply.github.com>
2023-07-07 20:08:39 +0530

    Time: 53 ms (36.08%), Space: 16 MB (99.78%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -2,6 +2,8 @@ import math
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         
+        
+        # Using Hash Tables to Calculate
         comb_map = {}
         
         for numb in nums:
@@ -17,6 +19,12 @@ class Solution:
                 count += 1
             if item> 2:
                 count += int(math.factorial(item)/(2 * math.factorial(item - 2)))
+                
+        # Using simple nested loops
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] == nums[j]:
+        #             count+=1
         
         return count
                 
